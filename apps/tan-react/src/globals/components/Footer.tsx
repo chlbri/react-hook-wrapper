3a280b6e82cde4648590c88adbffbc4dafa22d00
chlbri <51156116+chlbri@users.noreply.github.com>
2026-06-30 08:19:09 +0000

    chore: update copyright holder name in footer

--- apps/tan-react/src/globals/components/Footer.tsx
+++ apps/tan-react/src/globals/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className='mt-20 border-t border-slate-200 dark:border-slate-800 px-4 py-2 text-slate-500 dark:text-slate-400'>
       <div className='flex max-w-6xl mx-auto w-full justify-between items-center'>
         <p className='m-0 text-sm'>
-          &copy; {year} Your name here. All rights reserved.
+          &copy; {year} @chlbri. All rights reserved.
         </p>
         <MyGithub />
       </div>
